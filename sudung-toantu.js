81265fa145dc99c499a4591744b5c98d4c623d5b
phamthanh47 <54268953+phamthanh47@users.noreply.github.com>
2019-08-21 16:19:40 +0700

    thanh

---
+++ sudung-toantu.js
@@ -0,0 +1,8 @@
+let  inputWidth;
+let inputHeight;
+inputWidth = prompt("enter a width: ");
+inputHeight = prompt("enter a height: ");
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+let area = width * height;
+document.write("this area is: "+ area);
